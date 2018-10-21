8e70bff7d0f9d1f01ea2bc8bb8bcad2e3a3c814c
Gustavo Godoi <gugodoi+github@gmail.com>
2018-10-21 15:34:50 -0300

    /orders endpoint to return the clients and orders filtered to frontend

--- gateway-api/routes/index.js
+++ gateway-api/routes/index.js
@@ -3,16 +3,54 @@ const router = express.Router();
 const axios = require('axios');
 const request = require('request');
 
-router.get('/clients', async (req, res, next) => {
+async function getClients({ name, email, phone }) {
+  const url = `http://localhost:8081/v1/clients/search/byNamePhoneEmail?name=${name}&phone=${email}&email=${phone}`;
+  const response = await axios.get(url);
+  const { clients } = response.data._embedded;
+  return clients;
+}
+
+async function getOrders(ids, dateStart, dateEnd) {
+  const clientIds = ids || '';
+  const start = dateStart ? `&start=${dateStart}` : '';
+  const end = dateEnd ? `&end=${dateEnd}` : '';
+  let url;
+  if (clientIds) {
+    url = `http://localhost:8082/v1/orders/search/byClientIds?clientIds=${clientIds}${start}${end}`;
+  } else if (start || end) {
+    url = `http://localhost:8082/v1/orders/search/byDate?${start}${end}`;
+  } else {
+    url = `http://localhost:8082/v1/orders`;
+  }
+  const response = await axios.get(url);
+  const { orders } = response.data._embedded;
+  return orders;
+}
+
+router.get('/orders', async (req, res, next) => {
   try {
     const name = req.query.name || '';
     const email = req.query.email || '';
     const phone = req.query.phone || '';
-    const url = `http://localhost:8081/v1/clients/search/byNamePhoneEmail?name=${name}&phone=${email}&email=${phone}`;
-    const response = await axios.get(url);
-    const { clients } = response.data._embedded;
+
+    const clients = await getClients({ name, email, phone });
+
+    const clientIds = clients.map((client, index) => {
+      const selfId = client._links.self.href.split('/');
+      const id = selfId[selfId.length - 1];
+      clients[index].id = id;
+      return id;
+    });
+
+    const orders = await getOrders(clientIds, req.query.start, req.query);
+
+    const data = {
+      clients,
+      orders,
+    };
+
     res.setHeader('Content-Type', 'application/json');
-    res.send(clients);
+    res.send(data);
   } catch (e) {
     res.send(e);
   }
