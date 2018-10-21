5752e3787415b5c9df1491954955eae05aa6fc97
Gustavo Godoi <gugodoi+github@gmail.com>
2018-10-21 12:06:34 -0300

    New Order endpoint to filter by Ids and Date

--- micro-services/order-service/src/main/java/com/ifood/demo/order/OrderRepository.java
+++ micro-services/order-service/src/main/java/com/ifood/demo/order/OrderRepository.java
@@ -3,6 +3,7 @@ package com.ifood.demo.order;
 import java.util.Collection;
 import java.util.Date;
 import java.util.UUID;
+import java.util.List;
 
 import org.springframework.data.repository.CrudRepository;
 import org.springframework.data.repository.query.Param;
@@ -12,10 +13,13 @@ public interface OrderRepository extends CrudRepository<Order, UUID> {
 
 	@RestResource(path = "byClientId")
 	Collection<Order> findByClientId(@Param("clientId") UUID clientId);
-	
+
 	@RestResource(path = "byRestaurantId")
 	Collection<Order> findByRestaurantId(@Param("restaurantId") UUID restaurantId);
-	
+
 	@RestResource(path = "byDate")
 	Collection<Order> findByCreatedAtBetween(@Param("start") Date start, @Param("end") Date end);
+
+	@RestResource(path = "byClientIdsAndDate")
+	Collection<Order> findByClientIdInAndCreatedAtBetween(@Param("clientIds") List<UUID> clientId, @Param("start") Date start, @Param("end") Date end);
 }
